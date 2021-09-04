b5df391caa6607c28962c26dcad1f87ef30822b1
Chris Maughan <mornymorny@gmail.com>
2021-09-04 14:27:23 +0100

    Update

--- Render.cs
+++ Render.cs
@@ -5,12 +5,12 @@ namespace WindowsFormsApp1
 {
     public class Render
     {
-        Random rand = new Random();
+        private GameOfLife life = new GameOfLife();
 
-        public unsafe void DrawForeground(Graphics g)
+        public void DrawForeground(Graphics g)
         {
-            g.FillEllipse(new SolidBrush(Color.Red), new RectangleF(10, 10, 100, 100));
-            g.FillRectangle(new SolidBrush(Color.Green), new RectangleF(150, 150, 50, 50));
+            //g.FillEllipse(new SolidBrush(Color.Red), new RectangleF(10, 10, 100, 100));
+            //g.FillRectangle(new SolidBrush(Color.Green), new RectangleF(150, 150, 50, 50));
         }
 
         public unsafe void DrawBackground(Graphics g, Bitmap bmp)
@@ -19,19 +19,31 @@ namespace WindowsFormsApp1
             Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
             var lockData = bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.WriteOnly, bmp.PixelFormat);
 
+            life.InitCells(bmp.Width, bmp.Height);
+
+            life.Update();
+
+            int scale = life.GetScale();
+
             var ptr = (byte*)lockData.Scan0.ToPointer();
             for (int y = 0; y < bmp.Height; y++)
             {
                 for (int x = 0; x < bmp.Width; x++)
                 {
                     var pData = (UInt32*)(ptr + (y * lockData.Stride) + (x * 4));
-                    if (rand.Next(2) == 0)
+                    int val = life.GetCell(life.GetCurrentDest(), x / scale, y / scale); 
+
+                    if (val == 0)
+                    {
+                        *pData = 0xFF000000;
+                    }
+                    else if (val ==1 )
                     {
                         *pData = 0xFFFFFFFF;
                     }
-                    else
+                    else if (val ==2 )
                     {
-                        *pData = 0xFF000000;
+                        *pData = 0xFFFF0000;
                     }
                 }
             }
